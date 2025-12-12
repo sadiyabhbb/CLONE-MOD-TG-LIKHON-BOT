b9b0d4d29b7620b05dc21096859f6a67695daf69
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-12 16:22:21 +0600

    Delete config directory

--- config/botConfig.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  ADMIN_UID: parseInt(process.env.ADMIN_UID),      // তোমার Telegram UID
-  ADMIN_USERNAME: process.env.ADMIN_USERNAME       // তোমার Telegram username (without @)
-};
